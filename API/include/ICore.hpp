11e2086517dc4a3a723b0f42f8bb133e7a942937
PierreBougon <pierre.bougon@epitech.eu>
2017-10-31 17:04:11 +0100

    Prepare repository

---
+++ API/include/ICore.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Pierre Bougon on 31/10/17.
+//
+
+#ifndef CLIENTAI_ICORE_HPP_
+#define CLIENTAI_ICORE_HPP_
+
+namespace xzia
+{
+    class ICore
+    {
+        virtual void run() = 0;
+    };
+}
+
+#endif // !CLIENTAI_ICORE_HPP_
