04e157c61a8c5128b5ea3fb29fb41ec0543287eb
PierreBougon <pierre.bougon@epitech.eu>
2017-11-13 17:28:21 +0100

    Clean IHeader

--- API/include/IHeader.hpp
+++ API/include/IHeader.hpp
@@ -5,10 +5,12 @@
 #ifndef EXISTENZIA_IHEADER_HPP
 #define EXISTENZIA_IHEADER_HPP
 
+#include <string>
+
 namespace xzia {
-    class IHeader : {
+    class IHeader {
     public:
-        virtual void setField(std::string const & field, std::string const & value) 0;
+        virtual void setField(std::string const & field, std::string const & value) = 0;
         virtual std::string & getField(std::string const & field) const = 0;
         virtual void addField(std::string const & field) = 0;
         virtual void setField(std::string const & field) = 0;
