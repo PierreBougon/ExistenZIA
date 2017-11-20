8bf9c1fc96ca90ad1e29ef4f0b76f5eaa8e8c042
PierreBougon <pierre.bougon@epitech.eu>
2017-11-20 14:50:49 +0100

    Remove namespace data + use boost::variant as data storage base

--- API/include/modules/ADataStore.hpp
+++ API/include/modules/ADataStore.hpp
@@ -22,167 +22,166 @@
  * \class ADataStore ADataStore.hpp ADataStore.hpp
  *
  */
-namespace xzia {
-    namespace data {
+namespace xzia
+{
+    /**
+    * \enum DataType
+    */
+    enum class DataType : unsigned char{
+        CHAR,
+        INT,
+        FLOAT,
+        LONG,
+        DOUBLE,
+        STRING
+    };
+
+    /**
+    * \union DataValue
+    */
+    union DataValue {
+        char        c;
+        int         i;
+        float       f;
+        long        l;
+        double      d;
+        std::string s;
+    };
+
+    /**
+    * \struct Data
+    */
+    struct Data
+    {
+        DataType        type;
+        boost::variant  value;
+    };
+
+    class ADataStore {
+    protected:
+        std::map<std::string, Data> datas;
+    public:
 
         /**
-         * \enum DataType
-         */
-        enum class DataType : unsigned char{
-            CHAR,
-            INT,
-            FLOAT,
-            LONG,
-            DOUBLE,
-            STRING
-        };
+        *
+        * \fn addInt
+        * \brief Add data of type int
+        * @param i Data of type int
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addInt(int i, std::string const& key);
 
         /**
-         * \union DataValue
-         */
-        union DataValue {
-            char        c;
-            int         i;
-            float       f;
-            long        l;
-            double      d;
-            std::string s;
-        };
+        *
+        * \addChar
+        * \brief Add data of type char
+        * @param c Data of type char
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addChar(char c, std::string const& key);
 
         /**
-         * \struct Data
-         */
-        struct Data {
-            DataType    type;
-            variant   value;
-        };
+        *
+        * \fn addLong
+        * \brief Add data of type long
+        * @param l Data of type long
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addLong(long l, std::string const& key);
 
-        class ADataStore {
-        protected:
-            std::map<std::string, Data> datas;
-        public:
+        /**
+        *
+        * \fn addStr
+        * \brief Add data of type string
+        * @param s Data of type string
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addStr(std::string const &s, std::string const& key);
 
-            /**
-             *
-             * \fn addInt
-             * \brief Add data of type int
-             * @param i Data of type int
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addInt(int i, std::string const& key);
+        /**
+        *
+        * \fn addFloat
+        * \brief Add data of type float
+        * @param f Data of type float
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addFloat(float f, std::string const& key);
 
-            /**
-             *
-             * \addChar
-             * \brief Add data of type char
-             * @param c Data of type char
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addChar(char c, std::string const& key);
+        /**
+        *
+        * \fn addDouble
+        * \brief Add data of type double
+        * @param d Data of type double
+        * @param key Key associated to the data adding
+        *
+        */
+        void        addDouble(double d, std::string const& key);
 
-            /**
-             *
-             * \fn addLong
-             * \brief Add data of type long
-             * @param l Data of type long
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addLong(long l, std::string const& key);
+        /**
+        *
+        * \fn getInt
+        * \brief Get the data of type int asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type int determined by the key
+        *
+        */
+        int         getInt(std::string const& key) const;
 
-            /**
-             *
-             * \fn addStr
-             * \brief Add data of type string
-             * @param s Data of type string
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addStr(std::string const &s, std::string const& key);
+        /**
+        *
+        * \fn getChar
+        * \brief Get the data of type char asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type char determined by the key
+        *
+        */
+        char        getChar(std::string const& key) const;
 
-            /**
-             *
-             * \fn addFloat
-             * \brief Add data of type float
-             * @param f Data of type float
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addFloat(float f, std::string const& key);
+        /**
+        *
+        * \fn getLong
+        * \brief Get the data of type long asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type long determined by the key
+        *
+        */
+        long        getLong(std::string const& key) const;
 
-            /**
-             *
-             * \fn addDouble
-             * \brief Add data of type double
-             * @param d Data of type double
-             * @param key Key associated to the data adding
-             *
-             */
-            void        addDouble(double d, std::string const& key);
+        /**
+        *
+        * \fn getStr
+        * \brief Get the data of type string asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type string determined by the key
+        *
+        */
+        std::string &getStr(std::string const& key) const;
 
-            /**
-             *
-             * \fn getInt
-             * \brief Get the data of type int asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type int determined by the key
-             *
-             */
-            int         getInt(std::string const& key) const;
+        /**
+        *
+        * \fn getFloat
+        * \brief Get the data of type float asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type float determined by the key
+        *
+        */
+        float       getFloat(std::string const& key) const;
 
-            /**
-             *
-             * \fn getChar
-             * \brief Get the data of type char asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type char determined by the key
-             *
-             */
-            char        getChar(std::string const& key) const;
-
-            /**
-             *
-             * \fn getLong
-             * \brief Get the data of type long asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type long determined by the key
-             *
-             */
-            long        getLong(std::string const& key) const;
-
-            /**
-             *
-             * \fn getStr
-             * \brief Get the data of type string asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type string determined by the key
-             *
-             */
-            std::string &getStr(std::string const& key) const;
-
-            /**
-             *
-             * \fn getFloat
-             * \brief Get the data of type float asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type float determined by the key
-             *
-             */
-            float       getFloat(std::string const& key) const;
-
-            /**
-             *
-             * \fn getDouble
-             * \brief Get the data of type double asking
-             * @param key Key associated to the data adding
-             * @return Return the specific data of type double determined by the key
-             *
-             */
-            double      getDouble(std::string const& key) const;
-        };
-    }
+        /**
+        *
+        * \fn getDouble
+        * \brief Get the data of type double asking
+        * @param key Key associated to the data adding
+        * @return Return the specific data of type double determined by the key
+        *
+        */
+        double      getDouble(std::string const& key) const;
+    };
 }
 
 #endif //EXISTENZIA_ADATASTORE_HPP
